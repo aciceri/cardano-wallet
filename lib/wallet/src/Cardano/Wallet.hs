ddb3325f433a0600c37c19baa3ae6159b2ba4967
Pawel Jakubas <pawel.jakubas@iohk.io>
2023-02-09 17:34:57 +0100

    introduce dScriptTemplateM in balanceTransaction

--- lib/wallet/src/Cardano/Wallet.hs
+++ lib/wallet/src/Cardano/Wallet.hs
@@ -2021,7 +2021,8 @@ buildAndSignTransactionPure
                 nullTracer
                 txLayer
                 Nothing -- "To input scripts" resolver
-                Nothing -- Script template
+                Nothing -- Payment cript template
+                Nothing -- Delegation script template
                 nodeProtocolParameters
                 ti
                 (UTxOIndex.fromMap
